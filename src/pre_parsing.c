6815f7ce44023b4fa589e0d970e64542c11f0aee
Clement BERNARD <clbernar@paul-f5Br1s1.clusters.42paris.fr>
2023-11-02 13:56:55 +0100

    test

---
+++ src/pre_parsing.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pre_parsing.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clbernar <clbernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 13:26:46 by clbernar          #+#    #+#             */
+/*   Updated: 2023/11/02 13:40:45 by clbernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+// Cette fonction verifie que l'argument envoye est bien au format .ber
+// This function checks if map format is .cub or not
+int	map_format_ok(char *arg)
+{
+	int	i;
+	int	format_cub;
+
+	i = ft_strlen(arg) - 1;
+	format_cub = 0;
+	while (i != 0)
+	{
+		if (arg[i] == '.')
+		{
+			if (arg[i + 1] == 'c' && arg[i + 2] == 'u'
+				&& arg[i + 3] == 'b')
+				format_cub++;
+			if (format_cub != 0)
+			{
+				while (arg[i])
+				{
+					if (arg[i] != ' ')
+						return (0);
+					i++;
+				}
+				return (1);
+			}
+		}
+		i--;
+	}
+	ft_printf("L'argument fourni n'est pas au format adequat\n");
+	exit(EXIT_FAILURE);
+}

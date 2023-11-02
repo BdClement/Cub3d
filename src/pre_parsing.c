16c00a063e33d5f9d18a1e306c849c86335d1633
Bastien MIRLICOURTOIS <bmirlico@paul-f5Br5s3.clusters.42paris.fr>
2023-11-02 19:39:25 +0100

    pre-parsing merge

--- src/pre_parsing.c
+++ src/pre_parsing.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   pre_parsing.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmirlico <bmirlico@student.42.fr>          +#+  +:+       +#+        */
+/*   By: clbernar <clbernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:50:49 by clbernar          #+#    #+#             */
-/*   Updated: 2023/11/02 19:36:50 by bmirlico         ###   ########.fr       */
+<<<<<<< HEAD
+/*   Updated: 2023/11/02 19:32:35 by bmirlico         ###   ########.fr       */
+=======
+/*   Updated: 2023/11/02 19:23:36 by clbernar         ###   ########.fr       */
+>>>>>>> origin/Clement
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
+
+/*************PARTIE CLEMENT*********************/
 
 int	parsing_is_ok(char *arg)
 {
@@ -39,4 +45,32 @@ int	format_cub_ok(char *arg)
 	}
 	ft_printf("Error.\nArgument's format is not as expected.\n");
 	return (0);
+}
+
+/*************PARTIE BASTIEN*********************/
+
+int	file_exists_or_is_a_dir(char *str)
+{
+	int		fd;
+
+	fd = open(str, O_RDONLY | O_DIRECTORY);
+	if (fd >= 0)
+	{
+		printf("%s is a directory.\n", str);
+		if (close(fd) < 0)
+			exit(EXIT_FAILURE);
+		return (0);
+	}
+	fd = open(str, O_RDONLY, S_IRUSR);
+	if (fd == -1)
+	{
+		printf("%s doesn't exist or doesn't have the right permissions.\n", str);
+		return (0);
+	}
+	else
+	{
+		if (close(fd) < 0)
+			exit(EXIT_FAILURE);
+		return (1);
+	}
 }
